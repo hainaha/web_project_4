e16718fe9f3cdd5b211faebd66f1b0fbb2c33e9b
hainaha <hainaha@gmail.com>
2022-05-29 21:23:56 -0300

    Insere códigos javascript para a janela de editar perfil

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+let openButton = document.querySelector(".edit-button");
+let editProfile = document.querySelector(".edit-profile");
+let saveButton = document.querySelector(".edit-profile__save");
+let closeButton = document.querySelector(".edit-profile__close");
+
+function openEditProfile() {
+  editProfile.classList.toggle("edit-profile_opened");
+}
+
+function saveEditProfile() {
+  let inputName = document.querySelector("#input-name");
+  let inputAboutMe = document.querySelector("#input-aboutme");
+  let profileName = document.querySelector(".header__title");
+  let aboutMe = document.querySelector(".header__subtitle");
+  profileName.textContent = inputName.value;
+  aboutMe.textContent = inputAboutMe.value;
+  profileName.value = "";
+  aboutMe.value = "";
+  openEditProfile();
+}
+
+openButton.addEventListener("click", openEditProfile);
+closeButton.addEventListener("click", openEditProfile);
+saveButton.addEventListener("click", saveEditProfile);
