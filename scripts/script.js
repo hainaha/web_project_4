35c2ff4073ed0c24c138ff5609cc2490d3049a83
hainaha <hainaha@gmail.com>
2022-06-09 10:52:23 -0300

    feat: adiciona javascript para criação dos cards e botão curtir

--- scripts/script.js
+++ scripts/script.js
@@ -20,3 +20,57 @@ function saveEditProfile() {
 openButton.addEventListener("click", openEditProfile);
 closeButton.addEventListener("click", openEditProfile);
 saveButton.addEventListener("click", saveEditProfile);
+
+const initialCards = [
+  {
+    src: "./images/yosemite.png",
+    alt: "Foto de Yosemite",
+    title: "Yosemite Valley",
+  },
+  {
+    src: "./images/lake_louise.png",
+    alt: "Foto de Lake Louise",
+    title: "Lake Louise",
+  },
+  {
+    src: "./images/bald_mountains.png",
+    alt: "Foto de Bald Mountains",
+    title: "Bald Mountains",
+  },
+  {
+    src: "./images/latemar.png",
+    alt: "Foto de Latemar",
+    title: "Latemar",
+  },
+  {
+    src: "./images/vanoise_national_park.png",
+    alt: "Foto de Vanoise National Park",
+    title: "Vanoise National Park",
+  },
+  {
+    src: "./images/lago_di_braies.png",
+    alt: "Foto do Lago di Braies",
+    title: "Lago di Braies",
+  },
+];
+
+const cardTemplate = document.querySelector("#cards__template").content;
+const cardsContainer = document.querySelector(".content");
+initialCards.forEach((item) => {
+  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  cardElement.querySelector(".card__image").src = item.src;
+  cardElement.querySelector(".card__image").alt = item.alt;
+  cardElement.querySelector(".card__title").textContent = item.title;
+  cardsContainer.append(cardElement);
+});
+
+function like() {
+  const likeButton = document.querySelectorAll(".like-button");
+  likeButton.forEach((item) => {
+    item.addEventListener("click", function (evt) {
+      evt.target.classList.toggle("like-button_active");
+    });
+  });
+}
+
+like();
