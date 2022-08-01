73b77fd44239455e8da65719af9cecda9f67f904
hainaha <hainaha@gmail.com>
2022-08-01 10:48:29 -0300

    Limpa comentários e trechos não usados do código

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,3 @@
-import { PopupWithImage } from "../components/PopupWithImage";
-
 export default class Card {
   constructor({ data, cardSelector, handleCardClick }) {
     this._text = data.title;
@@ -24,19 +22,9 @@ export default class Card {
     this._element.querySelector(".card__image").src = this._image;
     this._element.querySelector(".card__image").alt = this._text;
     this._element.querySelector(".card__title").textContent = this._text;
-    // this._element.querySelector(".popup__expanded-image").src = this._image;
-    // this._element.querySelector(".popup__place-name").textContent = this._text;
 
     return this._element;
   }
-
-  // _openImagePopup() {
-  //   this._element
-  //     .querySelector(".popup_image")
-  //     .classList.toggle("popup_opened");
-  //   const imagePopup = new ImagePopup(this._element);
-  //   imagePopup.generatePopup(this._element);
-  // }
 
   _setEventListeners() {
     this._element
--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,15 +1,7 @@
 export default class Popup {
   constructor(popupSelector) {
     this._element = document.querySelector(popupSelector);
-    // this._openButtonSelector = openButtonSelector;
-    // this._closeButtonSelector = closeButtonSelector;
   }
-
-  // Classe Popup
-  // o	Parâmetro do construtor – seletor popup OK
-  // o	Métodos públicos que abrirão e fecharão o popup open() e close() OK
-  // o	Método privado _handleEscClose() – fechar com tecla esc
-  // o	Método público setEventListeners() – evento de click no botão de fechar e ao clicar fora da janela
 
   open() {
     this._element.classList.add("popup_opened");
@@ -18,22 +10,6 @@ export default class Popup {
   close() {
     this._element.classList.remove("popup_opened");
   }
-
-  generatePopup() {
-    // this._element = document.querySelector(this._popupSelector);
-    // this._setEventListeners();
-    this.setEventListeners();
-  }
-
-  // acho que tem que ir pro index
-
-  // _setEventListeners() {
-  //   document
-  //     .querySelector(this._openButtonSelector)
-  //     .addEventListener("click", () => {
-  //       this.open();
-  //     });
-  // }
 
   setEventListeners() {
     this._element
